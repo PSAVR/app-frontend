c3fc3de1587bd07d5b4cb385dbd1186151848844
Emily-MM <65458318+Emily-MM@users.noreply.github.com>
2026-01-12 10:20:07 -0500

    Cambios en API_BASE para deploy

--- pages/js/api-base.js
+++ pages/js/api-base.js
@@ -3,11 +3,14 @@
 
   const LOCAL_API = 'http://localhost:4000';
 
-  const PROD_API = 'https://BACKEND-API.com';
+  const PROD_API = null;
 
   const host = window.location.hostname;
   const isLocalhost = (host === 'localhost' || host === '127.0.0.1');
 
-  window.API_BASE = LOCAL_API;
-  // window.API_BASE = isLocalhost ? LOCAL_API : PROD_API;
+  window.API_BASE = isLocalhost ? LOCAL_API : PROD_API;
+
+  if (!window.API_BASE) {
+    console.warn("API_BASE no configurado para producción todavía.");
+  }
 })();
